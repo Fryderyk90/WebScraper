f3de208e5cff73676a9632643c320b0298ed4dd0
Frydeyk <65290472+Frydeyk@users.noreply.github.com>
2021-11-01 16:15:49 +0100

    dont know

---
+++ Scraper.lib/Webhallen/Interface/IData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Scraper.lib.Webhallen.Interface
+{
+    public interface IData
+    {
+        List<Item> AllItems();
+        List<Item> ItemsByName(string name);
+    }
+}
